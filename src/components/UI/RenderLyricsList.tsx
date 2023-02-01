4393364a139c2f54f501ea762e96ae3c1c363c78
tyler-Wiz <tylertooxclusive@gmail.com>
2023-02-01 22:45:19 +0000

    Mobile

--- src/components/UI/RenderLyricsList.tsx
+++ src/components/UI/RenderLyricsList.tsx
@@ -11,10 +11,10 @@ interface Props {
 const RenderLyricsList: FC<Props> = ({ data, header }) => {
   return (
     <>
-      <h2 className="px-10 mt-6 text-xl text-black dark:text-primary font-semibold font-Crimson capitalize">
+      <h2 className="px-5 md:px-10 mt-6 text-xl text-black dark:text-primary font-semibold font-Crimson capitalize">
         {header}
       </h2>
-      <div className="w-full mx-auto grid md:grid-cols-5 sm:grid-cols-3 gap-6 px-10 mt-6 font-Crimson dark:text-primary">
+      <div className="w-full mx-auto grid grid-cols-2 sm:grid-cols-5 gap-6 px-5 md:px-10 mt-6 font-Crimson dark:text-primary">
         {data.map((item) => (
           <div key={item.id} className="my-4">
             <Link href={`${"/lyrics/" + item.id}`}>
@@ -24,10 +24,12 @@ const RenderLyricsList: FC<Props> = ({ data, header }) => {
                 className="rounded-lg shadow-lg hover:scale-105 cursor-pointer"
               />
             </Link>
-            <p className="text-sm mt-4 font-medium text-black capitalize dark:text-primary">
+            <p className="text-sm sm:text-xs md:text-sm  mt-4 font-medium text-black capitalize dark:text-primary">
               {item.trackName}
             </p>
-            <p className="text-sm text-lightBlack">{item.artistName}</p>
+            <p className="text-sm sm:text-xs md:text-sm  text-lightBlack">
+              {item.artistName}
+            </p>
           </div>
         ))}
       </div>
