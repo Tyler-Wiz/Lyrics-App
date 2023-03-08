57f61a2ac93a15e88abbf29146931c084b9e081d
tyler-Wiz <tylertooxclusive@gmail.com>
2023-03-08 20:26:00 +0000

    Origin Bug issue

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,7 +13,7 @@ require("dotenv").config();
 const app = express();
 app.use(express.json({ limit: "50mb" }));
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
-app.use(cors());
+app.use(cors({ origin: true }));
 
 const port = process.env.PORT || 1000;
 const uri = process.env.MONGODB_URI;
