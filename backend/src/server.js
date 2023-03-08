47b4efa8b86954fe3fa5e618a929109c40f3bd08
tyler-Wiz <tylertooxclusive@gmail.com>
2023-03-08 21:00:11 +0000

    Cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -15,7 +15,7 @@ app.use(express.json({ limit: "50mb" }));
 
 app.use(
   cors({
-    origin: "https://tooxclusive.herokuapp.com/",
+    origin: "https://plug.tooxclusive.com/",
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
