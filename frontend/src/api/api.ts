1592d25fdc19f7d377ae27ae039ce8b43b698849
tyler-Wiz <tylertooxclusive@gmail.com>
2024-01-31 09:15:30 +0000

    [backend]remove procfile

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,3 +1,3 @@
-export const serverURL = "http://localhost:4001/";
+export const serverURL = process.env.NEXT_PUBLIC_BACKEND_URL;
 export const BaseURL = "http://localhost:4000/";
 export const clientURL = "http://localhost:3000/";
